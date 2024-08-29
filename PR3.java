c3617ad46e1f3ba512a5c7e3c774463f1153e7cf
Ashwin Rabari <166016321+AshwinDesai1@users.noreply.github.com>
2024-08-29 16:01:25 +0530

    Add files via upload

---
+++ PR3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pr3;
+class utility
+{
+  public static long factorial(long term)
+  {
+   
+  }
+}
+
+public class PR3 {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+        
+    }
+    
+}
